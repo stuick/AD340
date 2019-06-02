6761c3f1ebd08b6749a66555b9789cae1515485d
stuick <stuick@gmail.com>
2019-06-02 15:56:24 -0700

    HW5

--- HW5/app/src/androidTest/java/com/example/scorekeeper/ScorekeeperTest.java
+++ HW5/app/src/androidTest/java/com/example/scorekeeper/ScorekeeperTest.java
@@ -1,7 +1,12 @@
 package com.example.scorekeeper;
 
-import android.support.test.runner.AndroidJUnit4;
+import android.content.Intent;
+import android.os.Bundle;
 
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,14 +29,132 @@ import static org.hamcrest.CoreMatchers.is;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ScorekeeperTest {
+    @Before
+    public void setUp() {
+        Intent intent = new Intent(InstrumentationRegistry.getTargetContext(), MainActivity.class);
+        activityRule.launchActivity(intent);
+    }
+
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testGamePageValidateButton(){
+    public void testGamePageValidateButton_negative32Button(){
+        GamePage fragment = new GamePage();
+        Bundle bundle = new Bundle();
+        bundle.putString("player1", "testPlayer1");
+        bundle.putString("player2", "testPlayer2");
+        fragment.setArguments(bundle);
+
+        activityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.host_fragment, fragment)
+                .commit();
         onView(withId(R.id.p1_neg_32)).perform(click());
         onView(withId(R.id.p1Round)).check(matches(withText("-32")));
+
+        onView(withId(R.id.p2_neg_32)).perform(click());
+        onView(withId(R.id.p2Round)).check(matches(withText("-32")));
+
+
+
+        onView(withId(R.id.p1Round))
+                .perform(typeText("150"));
+        onView(withId(R.id.p1Round)).check(matches(withText("KO")));
+        onView(withId(R.id.p2Round)).check(matches(withText("WINNER")));
     }
+
+    @Test
+    public void testGamePageValidateButton_endGameTest(){
+        GamePage fragment = new GamePage();
+        Bundle bundle = new Bundle();
+        bundle.putString("player1", "testPlayer1");
+        bundle.putString("player2", "testPlayer2");
+        fragment.setArguments(bundle);
+
+        activityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.host_fragment, fragment)
+                .commit();
+
+
+
+        onView(withId(R.id.p1Round))
+                .perform(typeText("150"));
+
+        onView(withId(R.id.butt_validate)).perform(click());
+        onView(withId(R.id.p1Round)).check(matches(withText("KO")));
+        onView(withId(R.id.p2Round)).check(matches(withText("WINNER")));
+    }
+
+    public void testGamePageCancelLastButton(){
+        GamePage fragment = new GamePage();
+        Bundle bundle = new Bundle();
+        bundle.putString("player1", "testPlayer1");
+        bundle.putString("player2", "testPlayer2");
+        fragment.setArguments(bundle);
+
+        activityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.host_fragment, fragment)
+                .commit();
+
+        onView(withId(R.id.p1_neg_32)).perform(click());
+        onView(withId(R.id.p2Round))
+                .perform(typeText("100"));
+        onView(withId(R.id.butt_validate)).perform(click());
+
+        onView(withId(R.id.p1_neg_32)).perform(click());
+        onView(withId(R.id.p2Round))
+                .perform(typeText("40"));
+        onView(withId(R.id.butt_validate)).perform(click());
+
+        onView(withId(R.id.butt_cancel)).perform(click());
+        onView(withId(R.id.p1Round)).check(matches(withText("-32")));
+        onView(withId(R.id.p2Round)).check(matches(withText("100")));
+
+    }
+
+    public void testCreatePlayerNoName(){
+        GamePage fragment = new GamePage();
+        Bundle bundle = new Bundle();
+        bundle.putString("player1", "");
+        bundle.putString("player2", "");
+        fragment.setArguments(bundle);
+
+        activityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.host_fragment, fragment)
+                .commit();
+
+
+        onView(withId(R.id.first)).check(matches(withText("AHMED")));
+        onView(withId(R.id.second)).check(matches(withText("AICHA")));
+    }
+
+    public void testCreatePlayerNameGiven(){
+        GamePage fragment = new GamePage();
+        Bundle bundle = new Bundle();
+        bundle.putString("player1", "p1");
+        bundle.putString("player2", "p2");
+        fragment.setArguments(bundle);
+
+        activityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.host_fragment, fragment)
+                .commit();
+
+
+        onView(withId(R.id.first)).check(matches(withText("p1")));
+        onView(withId(R.id.second)).check(matches(withText("p2")));
+    }
+
+
 }
